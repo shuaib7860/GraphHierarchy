af0f3a938cdb8f78e27094b9e92e71a7cb47aeef
Shuaib <45211189+shuaib7860@users.noreply.github.com>
2019-11-24 13:55:10 +0000

    Add files via upload

--- GraphHierarchy.py
+++ GraphHierarchy.py
@@ -5,70 +5,59 @@ from scipy.sparse import coo_matrix, csr_matrix, diags
 from scipy.sparse.linalg import lsqr
 
 
-#The index postion of the element in the vector matches the networkx label of the node, so 0th element of vector is 0th labelled node. 
+#The index postion of the element in the vector matches the networkx label of the node, so 0th element of vector is the 0th labelled node. 
 def HierarchicalLevels(graph):
     A = nx.adjacency_matrix(graph).transpose()
-    k_in = A.sum(axis=1)
-    D_in = np.diagflat(k_in) #check to make sure this is a sparse matrix, it's not sparse
-    L_in = D_in - A
-    Lt_in = np.linalg.pinv(L_in)
-    s = Lt_in * k_in
-    return s
+    k_in = np.asarray(A.sum(axis=1))
+    D_in = np.diagflat(k_in)
+    L_in = D_in - A    
+    return np.linalg.lstsq(L_in, k_in)[0]
+
 # maybe return s as a dictionary with the key as the label of the node and the value is the gtl
-# Could implement linear solver for normal numpy array in above code
 
-
-# This is the more efficient piece of code, I think, need to confirm; confirmed 
-# k needs to be an array and so the csr function does have a toarray method but the normal numpy matrix object does not
 def HierarchicalLevelsSparse(graph):
     A = nx.adjacency_matrix(graph).transpose()
     k_in = csr_matrix(A.sum(axis=1))
     D_in = diags(A.sum(axis=1).A1, 0)
-    L_in = D_in - A
-    s = lsqr(L_in, k_in.toarray())
-    return s
+    L_in = D_in - A 
+    return lsqr(L_in, k_in.toarray())[0]
 
 
 def HierarchicalDifferences(graph):
     A = nx.adjacency_matrix(graph).transpose()
-    s = HierarchicalLevel(graph)
+    s = HierarchicalLevels(graph)
     
     TD = []
     for i, j in zip(A.nonzero()[0], A.nonzero()[1]):
         TD.append(s[i].item() - s[j].item())
     TDmatrix = coo_matrix((TD, (A.nonzero()[0], A.nonzero()[1])))
-    return TDmatrix.toarray()
-
-
-def HierarchicalLD(graph):
-    A = nx.adjacency_matrix(graph).transpose()
-    s = HierarchicalLevel(graph)
     
-    TD = []
-    for i, j in zip(A.nonzero()[0], A.nonzero()[1]):
-        TD.append(s[i].item() - s[j].item())
-    TDmatrix = coo_matrix((TD, (A.nonzero()[0], A.nonzero()[1])))
-    return s, TDmatrix.toarray()
+    return TDmatrix.toarray(), mean(TD), pstdev(TD)
 
 
 def HierarchicalCoherence(graph):
     A = nx.adjacency_matrix(graph).transpose()
-    s = HierarchicalLevel(graph)
+    s = HierarchicalLevels(graph)
+    
     TD = []
     for i, j in zip(A.nonzero()[0], A.nonzero()[1]):
         TD.append(s[i].item() - s[j].item())
-    return HierarchicalDifferences(graph), mean(TD), pstdev(TD)
+    TDmatrix = coo_matrix((TD, (A.nonzero()[0], A.nonzero()[1])))
+    
+    return s, TDmatrix.toarray(), mean(TD), pstdev(TD)
 
 
 
 # returns a measure of equitable controllability over the full graph/network
 def DemocracyCoefficient(graph):
     A = nx.adjacency_matrix(graph).transpose()
-    s = HierarchicalLevel(graph)
+    s = HierarchicalLevels(graph)
     TD = []
     for i, j in zip(A.nonzero()[0], A.nonzero()[1]):
         TD.append(s[i].item() - s[j].item())
     return 1 - mean(TD)
+
+# Just need to create sparse versions for the above function
 
 
 #Do we just want the mean of the hierarchical differences over a node or do we also want the set of differences
