e2d70fb6370e6dc1e504b85e8730a89884822eac
Shuaib <45211189+shuaib7860@users.noreply.github.com>
2020-04-12 21:40:28 +0100

    Add files via upload

--- GraphHierarchy/GraphHierarchy.py
+++ GraphHierarchy/GraphHierarchy.py
@@ -5,7 +5,7 @@ from scipy.sparse.linalg import lsqr
 
 
 def hierarchical_levels(graph, weight):
-    """Returns the hierarchical levels of the nodes of a network as an array in the ordering/indexing of graph.nodes().
+    """Returns the hierarchical levels of the nodes of a network as an array.
     
     Parameters
     ----------
@@ -14,12 +14,18 @@ def hierarchical_levels(graph, weight):
        
     weight :  string or None
         If you have no weighted edges insert weight=None, otherwise weight='string', where string is your underlying weight attribute
-           
+    
+    Returns
+    -------
+    hierarchical levels : array
+        A Nx1 dimensional array indexed by the nodes, in the same order as graph.nodes, holding the value of their hierarchical levels.
+    
     References
     ----------
     .. [1] Moutsinas, G., Shuaib, C., Guo, W., & Jarvis, S. (2019). 
     Graph hierarchy and spread of infections. 
     arXiv preprint arXiv:1908.04358."""
+    
     A = adjacency_matrix(graph, weight=weight).transpose()
     k_in = csr_matrix(A.sum(axis=1))
     D_in = diags(A.sum(axis=1).A1, 0)
@@ -28,8 +34,7 @@ def hierarchical_levels(graph, weight):
 
 
 def hierarchical_differences(graph, weight):
-    """Returns the non-zero hierarchical differences over the edges of a network in the form of an adjacency matrix with the 
-    ordering/indexing of graph.nodes(). 
+    """Returns the hierarchical differences over the edges of a network in the form of a weighted adjacency matrix
     
     Parameters
     ----------
@@ -38,13 +43,19 @@ def hierarchical_differences(graph, weight):
        
     weight :  string or None
         If you have no weighted edges insert weight=None, otherwise weight='string', where string is your underlying weight attribute
-        
+    
+    Returns
+    -------
+     hierarchical differences : array
+        A NxN dimensional array representing a weighted adjancency matrix, with the edge weights corresponding to the hierarchical differences. 
+        The column index represents the source node of the edge and the row index represents the destination node of the edge.
         
     References
     ----------
     .. [1] Moutsinas, G., Shuaib, C., Guo, W., & Jarvis, S. (2019). 
     Graph hierarchy and spread of infections. 
     arXiv preprint arXiv:1908.04358."""
+    
     A = adjacency_matrix(graph, weight=weight).transpose()
     s = hierarchical_levels(graph, weight=weight)
     TD = A.copy()
@@ -58,6 +69,7 @@ def hierarchical_differences(graph, weight):
 
 def sparse_hierarchical_differences(graph, weight):
     ''' Just a copy of the hierarchical differences function that returns the sparse matrix instead of the dense representation'''
+    
     A = adjacency_matrix(graph, weight=weight).transpose()
     s = hierarchical_levels(graph, weight=weight)
     TD = A.copy()
@@ -71,6 +83,7 @@ def sparse_hierarchical_differences(graph, weight):
 
 def sparse_matrix_mean(sparse_matrix):
     '''A mean calculation for sparse matrices that does not count the zero elements'''
+    
     if sparse_matrix.sum(dtype=float) == 0:
         return 0
     else:
@@ -78,8 +91,8 @@ def sparse_matrix_mean(sparse_matrix):
 
 
 def hierarchical_coherence(graph, weight):
-    """Returns the hierarchical differences over the edges of a network in the form of an adjacency matrix with the 
-    ordering/indexing of graph.nodes(), mean of the distribution of differences and standard deviation of this distribution.
+    """Returns the hierarchical differences over the edges of a network in the form of a weighted adjacency matrix,
+    mean of the distribution of differences and standard deviation of this distribution.
     
     Parameters
     ----------
@@ -88,13 +101,26 @@ def hierarchical_coherence(graph, weight):
        
     weight :  string or None
         If you have no weighted edges insert weight=None, otherwise weight='string', where string is your underlying weight attribute
+    
+     Returns
+    -------
+     hierarchical differences : array
+        A NxN dimensional array representing a weighted adjancency matrix, with the edge weights corresponding to the hierarchical differences. 
+        The column index represents the source node of the edge and the row index represents the destination node of the edge. 
+        an edge
         
+    mean hierarchical level : float
+        The mean of the distribution of hierarchical differences.
+    
+    hierarchical coherence : float
+        The standard deviation of the distribution of hierarchical differences.
         
     References
     ----------
     .. [1] Moutsinas, G., Shuaib, C., Guo, W., & Jarvis, S. (2019). 
     Graph hierarchy and spread of infections. 
     arXiv preprint arXiv:1908.04358."""
+    
     TD = sparse_hierarchical_differences(graph, weight=weight)
     std =  (sparse_matrix_mean(TD.power(2)) - sparse_matrix_mean(TD)**2)**0.5
     return TD.toarray(), sparse_matrix_mean(TD), std
@@ -102,7 +128,7 @@ def hierarchical_coherence(graph, weight):
 
 # Returns a measure of equitable controllability over the full graph/network
 def democracy_coefficient(graph, weight):
-    """Returns the democracy coeffcient of a graph, a topological metric.
+    """Returns the democracy coeffcient of a graph, a topological network metric.
     
     Parameters
     ----------
@@ -111,19 +137,23 @@ def democracy_coefficient(graph, weight):
        
     weight :  string or None
         If you have no weighted edges insert weight=None, otherwise weight='string', where string is your underlying weight attribute
-        
-        
+    
+    Returns
+    -------
+    democracy coefficient : float
+        democracy coefficient of a graph
     References
     ----------
     .. [1] Moutsinas, G., Shuaib, C., Guo, W., & Jarvis, S. (2019). 
     Graph hierarchy and spread of infections. 
     arXiv preprint arXiv:1908.04358."""
+    
     TD = sparse_hierarchical_differences(graph, weight=weight)
     return 1 - sparse_matrix_mean(TD)
 
 
 def influence_centrality(graph, weight):
-    """Returns the influence centrality of all nodes in the network as an array in the ordering/indexing of graph.nodes().
+    """Returns the influence centrality of the nodes of a network as an array.
     
     Parameters
     ----------
@@ -132,12 +162,18 @@ def influence_centrality(graph, weight):
        
     weight :  string or None
         If you have no weighted edges insert weight=None, otherwise weight='string', where string is your underlying weight attribute.
+    
+     Returns
+    -------
+    influence centrality : array
+        A Nx1 dimensional array indexed by the nodes, in the same order as graph.nodes, holding the value of their influence centralities.
         
     References
     ----------
     .. [1] Moutsinas, G., Shuaib, C., Guo, W., & Jarvis, S. (2019). 
     Graph hierarchy and spread of infections. 
     arXiv preprint arXiv:1908.04358."""
+    
     TD = sparse_hierarchical_differences(graph, weight=weight)
     m = zeros((TD.shape[0], 1))
     for i in range(TD.shape[0]):
@@ -146,7 +182,7 @@ def influence_centrality(graph, weight):
 
 
 def node_influence_centrality(graph, weight, node):
-    """Returns the influence centrality of a given node in the network with node indexing determined by graph.nodes().
+    """Returns the influence centrality of the given node in the network.
     
     Parameters
     ----------
@@ -156,12 +192,18 @@ def node_influence_centrality(graph, weight, node):
     weight :  string or None
         If you have no weighted edges insert weight=None, otherwise weight='string', where string is your underlying weight attribute
     
-    node : number or string
-        Label of the node as determined by the makeup of the graph.nodes() call.
+    node : number
+        Label of the node as determined by the indexing of the graph.nodes() call.
+    
+    Returns
+    -------
+    influence centrality : float
+        A node's influence centrality.
         
     References
     ----------
     .. [1] Moutsinas, G., Shuaib, C., Guo, W., & Jarvis, S. (2019). 
     Graph hierarchy and spread of infections. 
     arXiv preprint arXiv:1908.04358."""
+    
     return 1 - sparse_matrix_mean(sparse_hierarchical_differences(graph, weight)[node])
