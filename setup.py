0cbc2841b14ef177519549f2dc342d48c57efb45
Shuaib <45211189+shuaib7860@users.noreply.github.com>
2020-10-16 13:20:08 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 LONG_DESCRIPTION = """
 **GraphHierarchy** is a python package that calculates the hierarchical level of nodes in a network, the associated hierarchical differences for the edges and the hierarchical coherence of the network. 
-Hierarchical levels are the mathematical generalisation of the trophic analysis of networks. Trophic levels and hence trophic coherence can be defined only on networks with well defined sources, known as basal nodes. 
+Hierarchical analysis is the mathematical generalisation of the trophic analysis of networks. Trophic levels and hence trophic coherence can be defined only on networks with well defined sources, known as basal nodes. 
 Trophic coherence, a measure of a network’s hierarchical organisation, has been shown to be linked to a network’s structural and dynamical properties. Thus trophic analysis of networks had been restricted to the ecological domain, until now.  
 Graph Hierarchy is a python package that implements this mathematical generalisation that allows for analysis of all network structures via the trophic approach. 
 See Moutsinas, G., Shuaib, C., Guo, W., & Jarvis, S. (2019). Graph hierarchy and spread of infections. arXiv preprint arXiv:1908.04358 for more details.
@@ -13,14 +13,14 @@ See Moutsinas, G., Shuaib, C., Guo, W., & Jarvis, S. (2019). Graph hierarchy and
 setuptools.setup(
   name = 'GraphHierarchy',
   packages = ['GraphHierarchy'],   
-  version = '1.2',      
+  version = '1.3',      
   license='MIT',        
   description = 'A module calculating quantities related to a network metric known as trophic coherence but now generalised to all networks, see Moutsinas, G., Shuaib, C., Guo, W., & Jarvis, S. (2019). Graph hierarchy and spread of infections. arXiv preprint arXiv:1908.04358 for more details.',
   long_description = LONG_DESCRIPTION,
   author = 'Choudhry Shuaib',                   
   author_email = 'cshuaib@outlook.com',      
   url = 'https://github.com/shuaib7860/GraphHierarchy',   
-  download_url = 'https://github.com/shuaib7860/GraphHierarchy/archive/v1.2.tar.gz',
+  download_url = 'https://github.com/shuaib7860/GraphHierarchy/archive/v1.3.tar.gz',
   keywords = ['Graph Hierarchy', 'Trophic Coherence', 'Hierarchical Coherence', 'Trophic Levels', 'Hierarchical Levels', 'Influence Centrality', 'Democracy Coefficient'],
   install_requires=[
           'numpy',
